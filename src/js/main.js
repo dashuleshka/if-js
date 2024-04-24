d84325a26c0ab39b403c67033facefc3ec2f95c2
dashuleshka <lakmand3@gmail.com>
2024-04-24 16:59:37 +0300

    lesson-11: add filter-block

--- src/js/main.js
+++ src/js/main.js
@@ -59,3 +59,75 @@ data.forEach((dataItem) => {
     newDiv1.appendChild(newSpan2);
     blockEl.appendChild(newDiv1);
 })
+
+//lesson-11
+const adultEl = document.getElementById('adults');
+const childrenEl = document.getElementById('children');
+const roomsEl = document.getElementById('rooms');
+const decrEl1 = document.getElementById('decr-1');
+const incrEl1 = document.getElementById('incr-1');
+const incrEl2 = document.getElementById('incr-2');
+const decrEl2 = document.getElementById('decr-2');
+const decrEl3 = document.getElementById('decr-3');
+const incrEl3 = document.getElementById('incr-3');
+const filterPositionsBlock = document.querySelector('.filter-block')
+const additionalBlock = document.querySelector('.block-2');
+const inputBookingPositions = document.getElementById('booking-positions');
+
+let numberAdults = 1;
+let numberRooms = 1;
+let numberChildren = 0;
+
+function filterIncrement(category) {
+    return function () {
+        if (category === adultEl && numberAdults >= 1 && numberAdults < 30) {
+            numberAdults++;
+            category.innerHTML = numberAdults.toString();
+        }
+        if (category === roomsEl && numberRooms >= 1 && numberRooms < 30) {
+            numberRooms++;
+            category.innerHTML = numberRooms.toString();
+        }
+        if (category === childrenEl && numberChildren >= 0 && numberChildren < 10 && category === childrenEl) {
+            numberChildren++;
+            category.innerHTML = numberChildren.toString();
+            additionalBlock.style.cssText = `
+            display: flex;
+            display: visible;`
+        }
+    }
+}
+
+function filterDecrement(category) {
+    return function () {
+        if (category === adultEl && numberAdults > 1 && numberAdults < 31) {
+            numberAdults--;
+            category.innerHTML = numberAdults.toString();
+        }
+        if (category === roomsEl && numberRooms > 1 && numberRooms < 31) {
+            numberRooms--;
+            category.innerHTML = numberRooms.toString();
+        }
+        if (category === childrenEl && numberChildren > 0 && numberChildren < 11) {
+            numberChildren--;
+            category.innerHTML = numberChildren.toString();
+            if (numberChildren === 0) {
+                additionalBlock.style.cssText = `
+            display: flex;
+            display: none;`
+            }
+        }
+    }
+}
+
+inputBookingPositions.addEventListener('click', () => {
+    filterPositionsBlock.style.cssText = `
+    visibility: visible;
+    `
+})
+incrEl1.addEventListener('click', filterIncrement(adultEl));
+decrEl1.addEventListener('click', filterDecrement(adultEl));
+incrEl2.addEventListener('click', filterIncrement(childrenEl));
+decrEl2.addEventListener('click', filterDecrement(childrenEl));
+incrEl3.addEventListener('click', filterIncrement(roomsEl));
+decrEl3.addEventListener('click', filterDecrement(roomsEl));
