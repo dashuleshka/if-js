fa402c76555eda7fc4adaf3003909526d1a80ad6
dashuleshka <lakmand3@gmail.com>
2024-05-19 16:19:19 +0300

    lesson-12: receiving data from the server

--- src/js/main.js
+++ src/js/main.js
@@ -1,44 +1,17 @@
-const data = [
-  {
-    id: "71ce9eac-e9b9-44f0-a342-9ff0b565f3b7",
-    name: "Hotel Leopold",
-    city: "Saint Petersburg",
-    country: "Russia",
-    imageUrl:
-      "https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/hotel-leopold_mflelk.jpg",
-  },
-  {
-    id: "aa560608-a879-48a7-80b6-deff2806b250",
-    name: "Apartment Sunshine",
-    city: "Santa  Cruz de Tenerife",
-    country: "Spain",
-    imageUrl:
-      "https://res.cloudinary.com/intellectfox/image/upload/v1610379364/fe/apartment-sunshine_vhdlel.jpg",
-  },
-  {
-    id: "1d88fefe-edf1-4cda-844a-babbf29bb2b3",
-    name: "Villa Kunerad",
-    city: "Vysokie Tatry",
-    country: "Slowakia",
-    imageUrl:
-      "https://res.cloudinary.com/intellectfox/image/upload/v1610379365/fe/villa-kunerad_gdbqgv.jpg",
-  },
-  {
-    id: "a2bf824d-edd8-41f0-8b70-244334086ab4",
-    name: "Hostel Friendship",
-    city: "Berlin",
-    country: "Germany",
-    imageUrl:
-      "https://res.cloudinary.com/intellectfox/image/upload/v1610379364/fe/hostel-friendship_aw6tn7.jpg",
-  },
-];
-
 const hotelsContainer = document.getElementById("hotels-block");
-const createHomesBlock = (data) =>
-  data
-    .map(
-      ({ id, name, city, country, imageUrl }) =>
-        `<div id=${id} class="hotels-block__item">
+fetch("https://if-student-api.onrender.com/api/hotels/popular")
+  .then((response) => {
+    if (!response.ok) {
+      throw new Error(`${response.status} - ${response.statusText}`);
+    }
+    return response.json();
+  })
+
+  .then((data) => {
+    const hotelsHTML = data
+      .map(
+        ({ id, name, city, country, imageUrl }) =>
+          `<div id=${id} class="hotels-block__item">
               <div>
                 <img
                   src=${imageUrl}
@@ -52,15 +25,17 @@ const createHomesBlock = (data) =>
                 >${city}, ${country}</span
               >
             </div>`,
-    )
-    .join(" ");
+      )
+      .join(" ");
 
-hotelsContainer.innerHTML = `<div class="container hotels-block__guests-loves-padding">
+    hotelsContainer.innerHTML = `<div class="container hotels-block__guests-loves-padding">
         <h2 class="hotels-block__heading">Homes guests loves</h2>
         <div class="hotels-block__content">
-        ${createHomesBlock(data)}
+        ${hotelsHTML}
         </div>
         </div>`;
+  })
+  .catch((err) => console.log(err.message));
 
 //Filters
 const optionsData = {
@@ -115,7 +90,7 @@ const plusOne = (optionName) => {
   if (optionsData[optionName].value < optionsData[optionName].max) {
     optionsData[optionName].value++;
 
-    if (optionName === 'children') {
+    if (optionName === "children") {
       addChildrenAge();
     }
   }
@@ -142,7 +117,7 @@ const plusOne = (optionName) => {
 const minusOne = (optionName) => {
   if (optionsData[optionName].value > optionsData[optionName].min) {
     optionsData[optionName].value--;
-    if (optionName === 'children') {
+    if (optionName === "children") {
       removeChildrenAge();
     }
   }
@@ -197,10 +172,9 @@ const addChildrenAge = () => {
 const removeChildrenAge = () => {
   const ageChildrenBlock = document.getElementById("options-select-items");
   const lastSelectItem = document.querySelector(
-      '.options-select-item:last-child',
+    ".options-select-item:last-child",
   );
   ageChildrenBlock.removeChild(lastSelectItem);
 };
 
 filterWrapper.addEventListener("click", createOptionsDiv);
-
